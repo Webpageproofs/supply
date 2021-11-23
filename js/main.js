21c5fc5e19e0e81559e7b35e335608f51c7d9e97
Webpageproofs <94922323+Webpageproofs@users.noreply.github.com>
2021-11-24 02:10:39 +0700

    Supply

---
+++ js/main.js
@@ -0,0 +1,1 @@
+function ibg(){for(var e=document.querySelectorAll(".ibg"),r=0;r<e.length;r++)e[r].querySelector("img")&&(e[r].style.backgroundImage="url("+e[r].querySelector("img").getAttribute("src")+")")}ibg();
